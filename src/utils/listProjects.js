3704dfc0b6d53922907977d9866a676921905b60
daprionil <davidarenaoflol23@gmail.com>
2023-08-03 21:07:55 -0500

    Install Carrousel Library and Add All Images to List Projects

---
+++ src/utils/listProjects.js
@@ -0,0 +1,51 @@
+import DogestImg1 from '../assets/dogest_image_1.png';
+import RDSImage1 from '../assets/rds_image.png';
+
+const projects = [
+    {
+        name: 'El rincón del sabiondo',
+        description:'El rincón del sabiondo, una excelente plataforma para los amantes de la lectura; elije y compra los libros de tu elección.',
+        role:' Full Stack Web Developer',
+        github:'https://github.com/Marifuhr/el_rincon_del_sabiondo',
+        deploy:' el-rincon-del-sabiondo-eta.vercel.app ',
+        images:[
+            RDSImage1
+        ],
+        tecnologies:[
+            'PostgreSQL',
+            'Express JS',
+            'Sequelize',
+            'React JS',
+            'Redux',
+            'React Router',
+            'Chakra UI',
+            'Cloudinary',
+            'MercadoPago',
+            'Auth0',
+        ],
+        
+    },
+    {
+        name: 'Dogest',
+        description:'Es una plataforma con gestión de usuarios y rutas privadas en donde puedes como usuario; Buscar, Crear y agregar a tus favoritos las razas que más de tu gusten.',
+        images:[
+            DogestImg1
+        ],
+        role:' Full Stack Web Developer',
+        github:'https://github.com/daprionil/pi_dogs',
+        deploy:'pi-dogs-daprionil.vercel.app',
+        tecnologies:[
+            'PostgreSQL',
+            'Express JS',
+            'Sequelize',
+            'React JS',
+            'Redux',
+            'React Router',
+            'TailwindCSS',
+            'Firebase',
+        ],
+        
+    }
+]
+
+export default projects;
