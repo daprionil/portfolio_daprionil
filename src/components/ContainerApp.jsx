611ce4938b668c721c3f72ae5297bef9aad9e67f
David Santiago Bulla Diaz <lasparodias60@gmail.com>
2023-08-24 20:15:27 -0500

    first

--- src/components/ContainerApp.jsx
+++ src/components/ContainerApp.jsx
@@ -1,6 +1,6 @@
 function ContainerApp({children}) {
     return (
-        <div className="bg-slate-800 ">
+        <div className="bg-slate-800 [&>section:nth-child(1)]:pt-28">
             {children}
         </div>
     );
