98388554caef5c8e4d2970ab28f10908970d83db
daprionil <davidarenaoflol23@gmail.com>
2023-08-27 19:14:27 -0500

    Fixed Styles for padding top in All Pages

--- src/components/ContainerApp.jsx
+++ src/components/ContainerApp.jsx
@@ -1,6 +1,6 @@
 function ContainerApp({children}) {
     return (
-        <div className="bg-slate-800 [&>section:nth-child(1)]:pt-24">
+        <div className="bg-slate-800 md:[&>section:nth-child(1)]:pt-24 ">
             {children}
         </div>
     );
